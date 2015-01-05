a12c579b714a8f741613b8762faeae7175f9bf73
Justin Craft <justin@wearewildfang.com>
2015-01-05 13:49:13 -0800

    Init portfolio with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = '4e1f67500f6b5a8474cf01463cda7b2d909ce2f7002231d6a09dcca52ca5f939408ea38e6a9ae3a79d83e4a6da66727d980081c8d1c9e8b397b154ff8a145b87'
